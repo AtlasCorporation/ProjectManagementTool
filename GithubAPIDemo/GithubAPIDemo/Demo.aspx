6c28457e5db1580a0b3fd2d19b3067d341dc6871
Aleksi <Aleksi@DESKTOP-9U5VM2E>
2016-10-05 20:47:46 +0300

    Add demo for Github API

---
+++ GithubAPIDemo/GithubAPIDemo/Demo.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Demo.aspx.cs" Inherits="GithubAPIDemo.Demo" Async="true"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Github API demo</title>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Github username: <asp:TextBox ID="txtUser" runat="server"></asp:TextBox><br />
+        Repo name: <asp:TextBox ID="txtRepo" runat="server"></asp:TextBox><br />
+        <asp:Button ID="btnHae" runat="server" Text="Hae commit feedi" OnClick="btnHae_Click" /><br />
+        <asp:Label runat="server" Text="" ID="lblMessages" /><br />
+        <asp:label runat="server" Text="" ID="lblHeader"></asp:label>
+        <asp:label runat="server" Text="" ID="lblCommitFeed"></asp:label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ GithubAPIDemo/GithubAPIDemo/Demo.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Octokit;
+using System.Threading.Tasks;
+
+namespace GithubAPIDemo
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        private string user = "";
+        private string repo = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {          
+        }
+
+        public async Task GetDataFromGithub()
+        {
+            try
+            {
+                var client = new GitHubClient(new ProductHeaderValue("project-management-tool"));
+                var commitList = await client.Repository.Commit.GetAll(user, repo);
+                lblHeader.Text = "<h2>Löytyi " + commitList.Count + " committia:</h2>";
+                for (int i = 0; i < commitList.Count; i++)
+                {
+                    lblCommitFeed.Text += commitList[i].Commit.Message + "<br/>";
+                    lblCommitFeed.Text += commitList[i].Commit.Committer.Name + " committed on " + commitList[i].Commit.Committer.Date.ToString("MM/dd/yyyy HH:mm") + "<hr/>";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessages.Text = ex.Message;
+            }
+        }
+
+        protected void btnHae_Click(object sender, EventArgs e)
+        {
+            user = txtUser.Text;
+            repo = txtRepo.Text;
+            if (!string.IsNullOrEmpty(user) && !string.IsNullOrEmpty(repo))
+                RegisterAsyncTask(new PageAsyncTask(GetDataFromGithub));
+            else
+                lblMessages.Text = "Täytä kentät...";
+        }
+    }
+}
