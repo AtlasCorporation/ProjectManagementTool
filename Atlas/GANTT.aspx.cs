2eaa118def58c03f5ff801e3dff4efc90db3365a
prome <janne.mottola@hotmail.com>
2016-10-06 14:51:05 +0300

    Start project

---
+++ Atlas/GANTT.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GANTT : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            lblFooter.Text = ex.Message;
+        }
+    }
+}
