6fae6444a6aa698402a3cf0eb64670ecbcad7bee
Nissinen <frazento@hotmail.com>
2016-11-01 12:40:47 +0200

    Password hash + salt + pepper
    SALT COLUMN MISSING FROM DATABASE!

---
+++ WebForms/Main.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="username" runat="server"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="password" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <asp:Label ID="result" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Label ID="hashResult" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Button ID="createAcc" runat="server" Text="Log In" OnClick="createAcc_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebForms/Main.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+public partial class Main : System.Web.UI.Page
+{
+
+    pwMixer mixer = new pwMixer();
+    private string salt;
+    private string pw;
+    private byte[] pw_bytes;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void createAcc_Click(object sender, EventArgs e)
+    {
+        result.Text = password.Text;
+        salt = mixer.CreateSalt();
+        string pw = username.Text + salt + password.Text;
+        pw_bytes = ASCIIEncoding.ASCII.GetBytes(pw);
+        SHA512Managed sha512 = new SHA512Managed();
+
+        var hashed_byte_array = sha512.ComputeHash(pw_bytes);
+        hashResult.Text = Convert.ToBase64String(hashed_byte_array);
+    }
+}
