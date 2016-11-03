7b24f71204795755e89df5a687894e1c12b3a189
Nissinen <frazento@hotmail.com>
2016-11-03 17:41:27 +0200

    mysql user table fix, and some fetching

--- WebForms/Main.aspx
+++ WebForms/Main.aspx
@@ -9,15 +9,33 @@
 <body>
     <form id="form1" runat="server">
     <div>
+        <asp:Label ID="Label1" runat="server" Text="Username"></asp:Label>
         <asp:TextBox ID="username" runat="server"></asp:TextBox>
         <br />
+        <asp:Label ID="Label2" runat="server" Text="Password"></asp:Label>
         <asp:TextBox ID="password" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label7" runat="server" Text="Retype Password"></asp:Label>
+        <asp:TextBox ID="repassword" runat="server" TextMode="Password"></asp:TextBox>
         <br />
         <asp:Label ID="result" runat="server" Text="Label"></asp:Label>
         <br />
         <asp:Label ID="hashResult" runat="server" Text="Label"></asp:Label>
         <br />
-        <asp:Button ID="createAcc" runat="server" Text="Log In" OnClick="createAcc_Click" />
+        <asp:Button ID="createAcc" runat="server" Text="Create acc" OnClick="createAcc_Click" />
+    </div>
+    <div>
+        <asp:Label ID="Label3" runat="server" Text="Username"></asp:Label>
+        <asp:TextBox ID="usernamelogin" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Password"></asp:Label>
+        <asp:TextBox ID="passwordlogin" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Button ID="LogIn" runat="server" Text="Log In" OnClick="LogIn_Click" />
     </div>
     </form>
 </body>
--- WebForms/Main.aspx.cs
+++ WebForms/Main.aspx.cs
@@ -7,6 +7,9 @@ using System.Web.UI.WebControls;
 using System.Web.Security;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 public partial class Main : System.Web.UI.Page
 {
@@ -14,7 +17,9 @@ public partial class Main : System.Web.UI.Page
     pwMixer mixer = new pwMixer();
     private string salt;
     private string pw;
+    private string pwCheck;
     private byte[] pw_bytes;
+    private byte[] pw_bytes2;
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -23,13 +28,85 @@ public partial class Main : System.Web.UI.Page
 
     protected void createAcc_Click(object sender, EventArgs e)
     {
-        result.Text = password.Text;
-        salt = mixer.CreateSalt();
-        string pw = username.Text + salt + password.Text;
-        pw_bytes = ASCIIEncoding.ASCII.GetBytes(pw);
+        string usernameRegex = @"^[A-Za-z0-9]+$";
+        if (Regex.IsMatch(username.Text, usernameRegex) && password.Text == repassword.Text)
+        {
+            result.Text = password.Text + " and salt: " + salt;
+            salt = mixer.CreateSalt();
+            pw = username.Text + salt + password.Text;
+            pw_bytes = ASCIIEncoding.ASCII.GetBytes(pw);
+            SHA512Managed sha512 = new SHA512Managed();
+
+            var hashed_byte_array = sha512.ComputeHash(pw_bytes);
+            string hashedPassword = Convert.ToBase64String(hashed_byte_array);
+            hashResult.Text = hashedPassword;
+            //
+            //
+            //
+            //Username EXISTS tsekkaus
+            string ConnString = ConfigurationManager.ConnectionStrings["Mysli"].ConnectionString;
+            string insert = "Insert into user(username, password, salt) values('" + username.Text + "','" + hashedPassword + "','" + salt + "')";
+            string query = "select * from user where username='" + username.Text + "'";
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(ConnString);
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader MyReader = cmd.ExecuteReader();
+                if (MyReader.HasRows)
+                {
+                    Label5.Text = "löytyy jo";
+                }
+                else
+                {
+                    Label5.Text = "ei löydy";
+                }
+                conn.Close();
+            }
+            catch (Exception)
+            {
+
+            } // END OF USERNAME EXISTS
+
+            //ADDING USER
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(ConnString);
+                MySqlCommand MyCommand = new MySqlCommand(insert, conn);
+                MySqlDataReader MyReader;
+                conn.Open();
+                MyReader = MyCommand.ExecuteReader();
+                conn.Close();
+                Label5.Text = "Palaute lähetetty onnistuneesti.";
+            }
+            catch (Exception ex)
+            {
+                Label6.Text = ex.Message;
+            }
+            // END OF ADDING USER
+            //
+            //
+            //
+        }
+        else
+        {
+            result.Text = "Something went wrong";
+        }
+
+        
+    }
+
+    protected void LogIn_Click(object sender, EventArgs e)
+    {
+        result.Text = passwordlogin.Text;
+        salt = "fiuf3"; //mixer.CreateSalt();
+        pw = usernamelogin.Text + salt + passwordlogin.Text;
+        pw_bytes2 = ASCIIEncoding.ASCII.GetBytes(pw);
         SHA512Managed sha512 = new SHA512Managed();
 
-        var hashed_byte_array = sha512.ComputeHash(pw_bytes);
-        hashResult.Text = Convert.ToBase64String(hashed_byte_array);
+        var hashed_byte_array = sha512.ComputeHash(pw_bytes2);
+        Label5.Text = passwordlogin.Text + " and salt: " + salt;
+        Label6.Text = Convert.ToBase64String(hashed_byte_array);
+
     }
 }
