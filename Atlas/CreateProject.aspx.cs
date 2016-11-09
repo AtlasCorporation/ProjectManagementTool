708e4d2ac3e7afbfa4f9bc2e36e9b4a926bb12be
Aleksi <Aleksi@DESKTOP-9U5VM2E>
2016-11-09 23:24:23 +0200

    Get Github repos for user

--- Atlas/CreateProject.aspx.cs
+++ Atlas/CreateProject.aspx.cs
@@ -4,18 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Octokit;
+using System.Threading.Tasks;
 
 public partial class CreateProject : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // Set focus back to ProjectDesc after postback
+        if (IsPostBack)
+            Page.SetFocus(txtProjectDesc);
     }
 
     protected void btnCreateProject_Click(object sender, EventArgs e)
     {
         if (!string.IsNullOrWhiteSpace(txtProjectName.Text))
-            CreateNewProject(txtProjectName.Text, txtProjectDesc.Text, txtGithubUser.Text, txtGithubRepo.Text);
+            CreateNewProject(txtProjectName.Text, txtProjectDesc.Text, txtGithubUser.Text, ddlGithubRepo.Text);
+        else
+            lblMessages.Text = "Please enter name for your project.";
     }
 
     /// <summary>
@@ -45,6 +51,43 @@ public partial class CreateProject : System.Web.UI.Page
         catch (Exception ex)
         {
             lblMessages.Text = ex.Message;
+        }
+    }
+
+    protected void txtGithubUser_TextChanged(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(txtGithubUser.Text))
+            RegisterAsyncTask(new PageAsyncTask(GetReposForUser));
+        else
+            ddlGithubRepo.Items.Clear();
+    }
+
+    /// <summary>
+    /// Gets Github repositories for given user.
+    /// </summary>
+    public async Task GetReposForUser()
+    {
+        try
+        {
+            ddlGithubRepo.Items.Clear();
+            lblMessages.Text = "";
+            var client = new GitHubClient(new ProductHeaderValue("atlas"));
+            var repos = await client.Repository.GetAllForUser(txtGithubUser.Text);
+            if (repos != null && repos.Count > 0)
+            {
+                foreach (var r in repos)
+                {
+                    ddlGithubRepo.Items.Add(new ListItem(r.Name, r.Name));
+                }
+            }
+            else
+            {
+                lblMessages.Text = "Could not find any repositories for user '" + txtGithubUser.Text + "'.";
+            }
+        }
+        catch (Exception)
+        {
+            lblMessages.Text = "User '" + txtGithubUser.Text + "' does not exist.";
         }
     }
 }
