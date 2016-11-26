5abf7e5902c527c613b0039f1a218a222e542eb4
prome- <janne.mottola@hotmail.com>
2016-11-26 23:42:52 +0200

    Develop gantt

--- Atlas/App_Code/SiteLogic.cs
+++ Atlas/App_Code/SiteLogic.cs
@@ -24,11 +24,10 @@ public class SiteLogic
         for (int i = 0; i < tasks.Count; i++)
         {
             tempTask = tasks.ElementAt(i);
-            tasksJson += "{id:" + tempTask.GanttId + @", text:""" + tempTask.Text + @""", start_date:""" + tempTask.StartDate + @""", duration:" + tempTask.Duration;
-
+            tasksJson += "{id:" + tempTask.GanttId;
             if (tempTask.Parent != null)
             {
-                tasksJson += ", parent:" + tempTask.GanttParentId + "}";
+                tasksJson += @", text:""" + tempTask.Text + @""", start_date:""" + tempTask.StartDate + @""", duration:" + tempTask.Duration + ", parent:" + tempTask.GanttParentId + "}";
             }
             else
             {
@@ -45,6 +44,134 @@ public class SiteLogic
         return tasksJson;
     }
 
+    public static string GetTasksJson(int projectId)
+    {
+        IEnumerable<donetask> donetasks;
+        IEnumerable<task> result = Database.GetProjectTasks(projectId);
+        List<task> tasks = result.ToList();
+        List<Task> parsedTasks = new List<Task>();
+
+
+            for(int i = 0;i<tasks.Count();i++)
+            {
+                if(tasks.ElementAt(i).task_id == null)
+                {
+                    parsedTasks.Add(new Task(tasks.ElementAt(i).id, tasks.ElementAt(i).name));
+                    tasks.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            bool hasChild = false;
+        
+
+            while (tasks.Count > 0 )
+            {
+                for (int i = 0; i < tasks.Count; i++)
+                {
+                    for(int j = 0; j<parsedTasks.Count;j++)
+                    {
+                        // onko vanhempaa
+                        if (tasks.ElementAt(i).task_id == parsedTasks.ElementAt(j).ID)
+                        {
+                            for(int k = 0;k<tasks.Count;k++)
+                            {
+                                // onko lapsia
+                                if(tasks.ElementAt(i).id == tasks.ElementAt(k).task_id)
+                                {
+                                    // on, siirretään task parsittuihin ilman tuntitietoja
+                                    parsedTasks.Add(new Task(tasks.ElementAt(i).id, tasks.ElementAt(i).name, parsedTasks.ElementAt(j).ID));
+                                    tasks.RemoveAt(i);
+                                    if (i == tasks.Count) { i--; }
+                                    if (tasks.Count == 0) { break; }
+                                    hasChild = true;
+                                    break;
+                                }
+                            }
+                            if (tasks.Count == 0) { break; }
+                            if (!hasChild)
+                            {
+                                // ei lapsia, lisätään parsittuihin donetaskeina
+                                donetasks = Database.GetDonetasks(tasks.ElementAt(i).id);
+                                foreach(donetask dt in donetasks)
+                                {
+                                    parsedTasks.Add(new Task(tasks.ElementAt(i).id, tasks.ElementAt(i).name, dt.date.Value.Day + "-" + dt.date.Value.Month + "-" + dt.date.Value.Year, dt.worktime, parsedTasks.ElementAt(j).ID));
+                                }
+                                tasks.RemoveAt(i);
+                                if(i == tasks.Count) { i--; }
+                                if (tasks.Count == 0) { break; }
+                            }
+                            hasChild = false;
+                        }
+                    }
+                    if(i < -1)
+                    {
+                        i = -1;
+                    }
+                    if (tasks.Count == 0) { break; }
+                }
+            }
+            // lisätään tilapäiset id:t ganttia varten
+            int x = 1;
+            foreach(Task item in parsedTasks)
+            {
+                item.GanttId = x;
+                x++;
+            }
+
+            foreach(Task item in parsedTasks)
+            {
+                if(item.Parent != null)
+                {
+                    item.GanttParentId = (from c in parsedTasks where item.Parent == c.ID select c.GanttId).FirstOrDefault();
+                }
+            }
+
+            return ParseTasksIntoJson(parsedTasks);        
+        
+
+    }
+
+    protected static string ParseTasksIntoJson(List<Task> tasks)
+    {
+        try
+        {
+            Task temptask;
+            string json = "{data:[";
+            for(int i = 0;i<tasks.Count;i++)
+            {
+                temptask = tasks.ElementAt(i);
+                if (temptask.StartDate == null)
+                {
+                    json += "{id:" + temptask.GanttId + @", text:""" + temptask.Text;
+                    if(temptask.Parent != null)
+                    {
+                        json += @""",parent:" + temptask.GanttParentId + "}";
+                    }
+                    else
+                    {
+                        json += @"""}";
+                    }
+                    
+                }
+                else
+                {
+                    json += "{id:" + temptask.GanttId + @", text:""" + temptask.Text + @""",start_date:""" + temptask.StartDate + @""",duration:" + temptask.Duration + ", parent:" + temptask.GanttParentId + "}";
+                }
+                if(i != tasks.Count -1)
+                {
+                    json += ",";
+                }
+            }
+            json += "]}";
+            return json;
+        }
+        catch (Exception ex)
+        {
+
+            throw ex;
+        }
+    }
 
 
     public static List<TaskNode> GetTaskNodes(int projectID)
@@ -67,6 +194,7 @@ public class SiteLogic
             }
         }
 
+        
         // arrange rest of the list into nodetrees
         while (tasks.Count > 0)
         {
@@ -75,14 +203,18 @@ public class SiteLogic
             {
                 for (int x = 0; x < nodes.Count; x++)
                 {
+                    System.Diagnostics.Debug.WriteLine("i:" + i + "x:" + x);
+                    System.Diagnostics.Debug.WriteLine("tasks.Count:" +  tasks.Count);
                     tempNode = new TaskNode(tasks.ElementAt(i).id.ToString(), tasks.ElementAt(i).name, tasks.ElementAt(i).task_id.ToString());
                     added = nodes.ElementAt(x).TryAddToParent(tempNode);
                     if (added)
                     {
                         tasks.RemoveAt(i);
+                        if(i == tasks.Count) { i--; }
                         if (tasks.Count == 0) { break; }
                     }
                 }
+                if (tasks.Count == 0) { break; }
             }
             // make sure the loop is not infinite
             if (tasks.Count == initialCount)
